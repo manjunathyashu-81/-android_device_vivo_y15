680559c273e9a66887f4d368c09758ecb9e2351b
kishpatel1998 <kishannn.p.19@gmail.com>
2017-09-21 09:58:28 +0000

    holly : Enforce vendor presence

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/huawei/holly/holly-vendor.mk)
+$(call inherit-product, vendor/huawei/holly/holly-vendor.mk)
 
 LOCAL_PATH := device/huawei/holly
 
