8c7dd3f6ee9978327b0862517c328d2deb9c1258
manjunath yashu <35634330+manjunathyashu-81@users.noreply.github.com>
2018-01-21 16:20:51 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter holly,$(TARGET_DEVICE)),)
+ifneq ($(filter Y15,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
